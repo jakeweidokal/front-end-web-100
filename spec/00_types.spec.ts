843603e13520b00c8f6608d2063bcdb6ad3bf9e0
Jacob Weidokal <35669473+jakeweidokal@users.noreply.github.com>
2020-05-18 18:42:01 +0000

    monday afternoon

---
+++ spec/00_types.spec.ts
@@ -0,0 +1,9 @@
+describe('types in typescript', () => {
+    describe('declaring variables and constants', () => {
+
+        it('implicitly any', () => {
+            // No Typescript in this example
+            
+        });
+    });
+});
