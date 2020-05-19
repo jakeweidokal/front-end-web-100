552e1bdd93443b6ff82d07b82a566c78e55232bc
Jacob Weidokal <35669473+jakeweidokal@users.noreply.github.com>
2020-05-19 19:57:20 +0000

    end of day 2 [functions]

--- spec/00_types.spec.ts
+++ spec/00_types.spec.ts
@@ -111,5 +111,162 @@ The end.`;
             expect(description2).toBe('The name is Joe and Joe is 51 and works as a DEV');
 
         });
+
+        it('has various ways to express numeric literals', () => {
+            let age: number;
+            age = 51;
+
+            const n1 = 1.2;
+            const n2 = 0xff // Base 16
+            const n4 = 0b010101 // Base 2 (binary)
+            const n5 = 0o23 // Base 8
+
+            // Can separate large numbers with underscores for readability, but they are not included in the actual value
+            const reallyBigNumber = 1_222_333_444;
+            expect(reallyBigNumber).toBe(1222333444);
+        });
+
+        describe('arrays and array literals', () => {
+
+            it('has two ays to declare an array', () => {
+                let stuff: (number | string)[];
+                stuff = ['dog', 'cat', 'mouse', 99];
+                expect(stuff[0]).toBe('dog');
+                expect(stuff[999]).toBeUndefined();
+
+                let otherStuff: Array<number | string>;
+                otherStuff = [1, 'bird', 99];
+            });
+
+            it('has array destructuring and a rest operator', () => {
+                const friends = ['sean', 'amy', 'david', 'henry'];
+
+                // const friend1 = friends[0]
+                // const friend3 = friends[2]
+                const [friend1, , friend3] = friends;
+
+                expect(friend1).toBe('sean');
+                expect(friend3).toBe('david');
+
+                // the rest operator
+                const [first, ...allTheOthers] = friends;
+
+                expect(first).toBe('sean');
+                expect(allTheOthers).toEqual(['amy', 'david', 'henry']);
+
+                const numbers = [1, 2, 3, 4, 5];
+                const newNumbers = [0, ...numbers, 6];
+
+                expect(newNumbers).toEqual([0, 1, 2, 3, 4, 5, 6]);
+
+                function addThemAll(...nums: number[]) {
+                    return nums.reduce((s, n) => s + n);
+                }
+
+                expect(addThemAll(1)).toBe(1);
+                expect(addThemAll(2, 2)).toBe(4);
+                expect(addThemAll(1, 2, 3, 4, 5, 6, 7, 8, 9)).toBe(45);
+            });
+
+            it('tuples', () => {
+                // typed arrays
+                const stuff: [string, number, string] = ['Cat', 13, 'Dog'];
+                const first = stuff[0];
+                const second = stuff[1];
+
+                type QuoteMarkRule = [boolean, 'single' | 'double'];
+
+                const myQuoteRule: QuoteMarkRule = [true, 'double'];
+                if (myQuoteRule[0]) {
+                    console.log(`You are enforcing quote marks and using ${myQuoteRule[1]} quotes`);
+                }
+            });
+
+            describe('a practical example of what you might use a tuple for (but probably would not)', () => {
+                it('an oop approach', () => {
+                    // string FormatName(string first, string last)
+                    interface FormattedName { formattedName: string; numberOfLettersInName: number; }
+                    function formatName(first: string, last: string): FormattedName {
+                        const formattedName = `${last}, ${first}`
+                        return {
+                            formattedName,
+                            numberOfLettersInName: formattedName.length
+                        }
+                    }
+                    function formatNameCasually(first: string, last: string): FormattedName {
+                        const formattedName = `${first} ${last}`;
+                        return {
+                            formattedName,
+                            numberOfLettersInName: formattedName.length
+                        }
+                    }
+                    const result = formatName('Han', 'Solo');
+                    expect(result.formattedName).toBe('Solo, Han');
+                    expect(result.numberOfLettersInName).toBe(9);
+                    // const result2 = formatNameCasually('Luke', 'Skywalker');
+                    // expect(result2.formattedName).toBe('Luke Skywalker');
+                    const { formattedName: n } = formatNameCasually('Luke', 'Skywalker');
+                    expect(n).toBe('Luke Skywalker');
+                });
+
+                it('if that wasn\'t confusing enough, here is tuples', () => {
+                    function formatName(first: string, last: string): [string, number] {
+                        const formattedName = `${last}, ${first}`;
+                        return [formattedName, formattedName.length]
+                    }
+                    const results = formatName('Han', 'Solo');
+                    expect(results[0]).toBe('Solo, Han');
+                    expect(results[1]).toBe(9);
+                    // but wait! We have destructuring
+                    const [fullName] = formatName('Luke', 'Skywalker');
+                    expect(fullName).toBe('Skywalker, Luke');
+                });
+
+                it('destructuring an object', () => {
+                    const movie = { title: 'A New Hope', director: 'Lucas', yearReleased: 1977 };
+
+                    // Old Skool
+                    const t1 = movie.title;
+                    const y1 = movie.yearReleased;
+                    expect(t1).toBe('A New Hope');
+                    expect(y1).toBe(1977);
+
+                    // new Skool
+                    const { title: t2, yearReleased: y2 } = movie;
+                    expect(t2).toBe('A New Hope');
+                    expect(y2).toBe(1977);
+                });
+
+                it('anonymous types are implicitly defined by an interface', () => {
+                    const thor: any = {
+                        title: 'Thor: Ragnarok',
+                        director: 'Taika Waititi',
+                        yearReleased: 2017
+                    };
+                    thor.title = 'Thor Ragnorok';
+                    expect(thor.title).toBe('Thor Ragnorok');
+                    // tslint:disable-next-line: no-string-literal
+                    expect(thor['title']).toBe('Thor Ragnorok');
+                    thor.yearreleased = 2017;
+                    thor.nicehair = true;
+                });
+
+                it('has duck typing (aka structural typing)', () => {
+                    interface MessageHaver { message: string }
+                    function logMessage(item: MessageHaver) {
+                        console.log(`At ${new Date().toLocaleTimeString()} you got the message ${item.message}`);
+                    }
+
+                    logMessage({ message: 'Call your mom' });
+
+                    const phoneCall = {
+                        from: 'Jenny',
+                        number: '867-5309',
+                        message: 'For a good time...'
+                    };
+                    logMessage(phoneCall);
+                });
+            });
+        });
     });
 });
